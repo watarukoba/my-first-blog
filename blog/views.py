f401a64430b635e41bb8a4be8f51ede031daf25f
wkoba <wkoba@mail.dendai.ac.jp>
2020-07-10 08:18:29 +0900

    Changes the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+    """
+    request を引数に取ります。
+    render という関数を呼び出して
+    blog/post_list.html テンプレートを（色々なものを合わせて）組み立てる 得た値を return しています。
+    """
